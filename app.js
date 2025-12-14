9837a4fa3c28dc70cc9824bc7275d2e1a7ffd453
Faisal Altaf <altaffaisal532@gmail.com>
2025-12-14 18:07:52 +0500

    Added the Delete Route

--- app.js
+++ app.js
@@ -33,6 +33,20 @@ app.use('', createPost);
 app.use('', postS)
 app.use('', updatedPost)
 
+app.delete('/deletePost/:id', async (req, res) => {
+    try {
+        const post = await Blog.findByIdAndDelete(req.params.id);
+        if (!post) {
+            return res.status(404).send('Post not found');
+        }
+        res.json(post,"Post Deleted Successfully");
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server Error');
+    }
+});
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
