24b8fa2ad4e091d55b184d7d3005151388c93a54
Faisal Altaf <altaffaisal532@gmail.com>
2025-12-15 16:29:49 +0500

    Made hte Update Post Secure for only the post owners

--- routes/updatePost.js
+++ routes/updatePost.js
@@ -4,12 +4,21 @@ const updatedPost = express();
 const protect = require('../middleware/auth');
 
  updatedPost.put('/updatepost/:id',protect, async (req, res) => {
+
+    const blog = await Blog.findById(req.params.id);
+    
+    if(blog.user.toString() !== req.user._id.toString()){
+      res.status(401);
+      throw new Error('User not authorized');
+    }
+
   try {
     const postId = req.params.id;
     const { title, description } = req.body;
     const findBlog = await Blog.findById(postId);
 
     console.log(findBlog);
+    
    const updatedPost = await Blog.findByIdAndUpdate(postId, { title, description }, { new: true });
     res.json(updatedPost);
     
