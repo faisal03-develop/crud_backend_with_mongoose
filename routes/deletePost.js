bc69250355afbeb065321196b9b4ff24f6115b93
Faisal Altaf <altaffaisal532@gmail.com>
2025-12-15 12:59:37 +0500

    User can Delete the Post from the Ui

--- routes/deletePost.js
+++ routes/deletePost.js
@@ -3,12 +3,13 @@ const Blog = require('../models/blog.model');
 const delet = express();
 
 
-delet.delete('/deletePost/:id', async (req, res) => {
+delet.delete('/deletepost/:id', async (req, res) => {
     try {
         const post = await Blog.findByIdAndDelete(req.params.id);
         if (!post) {
             return res.status(404).send('Post not found');
         }
+        // res.confirm('Do you want to delete the post?')
         res.json(post,"Post Deleted Successfully");
     } catch (err) {
         console.error(err);
--- routes/updatePost.js
+++ routes/updatePost.js
@@ -3,14 +3,13 @@ const Blog = require('../models/blog.model');
 const updatedPost = express();
 const protect = require('../middleware/auth');
 
- updatedPost.put('/updatepost/:id',protect, async (req, res) => {
+ updatedPost.put('/updatepost/:id', async (req, res) => {
   try {
     const postId = req.params.id;
     const { title, description } = req.body;
     const findBlog = await Blog.findById(postId);
 
     console.log(findBlog);
-    
    const updatedPost = await Blog.findByIdAndUpdate(postId, { title, description }, { new: true });
     res.json(updatedPost);
     
